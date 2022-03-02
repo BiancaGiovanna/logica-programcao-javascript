d57d4faddc5ac8fe01111d627708017645406dea
BiancaGiovanna <biancagothicg@gmail.com>
2022-03-02 18:53:10 -0300

    logica de programacao com JS

---
+++ operadoresLogicos/index.js
@@ -0,0 +1,11 @@
+/*
+&& -> AND -> E -> todas as expressões precisam ser verdadeiras para retornar verdadeiro
+|| -> OR -> OU -> 
+! -> NOT -> NÃO
+*/
+
+const expressionAnd = true && true
+
+const expressionOr = true || false;
+
+console.log(expressionOr);
